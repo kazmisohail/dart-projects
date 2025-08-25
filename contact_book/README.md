38d8e5db487c7329c8b6672a5a8888ee044b3b33
Sohail <152068955+kazmisohail@users.noreply.github.com>
2025-08-25 18:55:22 +0500

    first project folder created

---
+++ contact_book/README.md
@@ -0,0 +1,6 @@
+Contact Book
+Console based command line application with some basic CRUD functions.
+Add contact
+Delete contact
+Update contact
+Search contact
---
+++ contact_book/bin/contact_book.dart
@@ -0,0 +1,5 @@
+import 'package:contact_book/contact_book.dart' as contact_book;
+
+void main(List<String> arguments) {
+  print('Hello world: ${contact_book.calculate()}!');
+}
